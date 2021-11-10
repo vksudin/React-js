33b47dada8147ce3b940dfec8bca97e06efc30c0
sudin jana <69323964+vksudin@users.noreply.github.com>
2021-11-11 03:44:02 +0530

    Create Navbar.js

---
+++ Restaurant website/Navbar.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+const Navbar = ({filterItem}) => {
+  return (
+    <>
+      <nav className="navbar">
+        <div className="btn-group">
+          <button
+            className="btn-group__item"
+            onClick={() => filterItem("breakfast")}
+          >
+            BreakFast
+          </button>
+          <button
+            className="btn-group__item"
+            onClick={() => filterItem("lunch")}
+          >
+            Lunch
+          </button>
+          <button
+            className="btn-group__item"
+            onClick={() => filterItem("evening")}
+          >
+            Evening
+          </button>
+          <button
+            className="btn-group__item"
+            onClick={() => filterItem("dinner")}
+          >
+            Dinner
+          </button>
+          
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
