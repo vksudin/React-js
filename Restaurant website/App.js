5328b2d527cd16cd62cf7a3f42f7e9d894a8809e
sudin jana <69323964+vksudin@users.noreply.github.com>
2021-11-11 03:41:54 +0530

    Create App.js

---
+++ Restaurant website/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Restaurant from './Component/Restaurant'
+
+
+const App = () => {
+  return (
+    <>
+      <Restaurant></Restaurant>
+    </>
+  )
+}
+
+export default App
